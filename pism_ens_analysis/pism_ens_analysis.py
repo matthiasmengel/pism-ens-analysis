63e50d8226fee6d840b4f6daaf8c489f93962281
Matthias Mengel <mengel@pik-potsdam.de>
2017-07-28 16:46:49 +0200

    speed up rms sum calculation.

--- pism_ens_analysis/pism_ens_analysis.py
+++ pism_ens_analysis/pism_ens_analysis.py
@@ -12,12 +12,11 @@ def get_spatial_variable(fname,varname):
     return np.squeeze(ncf.variables[varname])
 
 
-def get_spatial_rms_error(variable,refvariable,mask,
-                          restrict_to="both"):
+def get_rms_error(variable,refvariable,mask, spatial=True):
 
     """ get the root mean square error between variable and
-    refvariable. Optional: do rms calculation only on floating
-    or grounded area, as determined from mask.
+    refvariable. Also get the sum of the rms for
+    grounded+floating, and grounded and floating alone.
     mask=2 is grounded,
     mask=3 is floating,
     mask=4 is ocean.
@@ -28,36 +27,34 @@ def get_spatial_rms_error(variable,refvariable,mask,
     rms = np.sqrt((variable-refvariable)**2.)
 
     ## do not count where now is ocean (could have been icy in ref)
+    ## FIXME: is this a reasonable decision?
     rms[mask==4] = 0.
 
-    if restrict_to == "floating":
-        rms[mask==2] = 0.
+    if spatial:
+        return rms
 
-    elif restrict_to == "grounded":
-        rms[mask==3] = 0.
+    rms_floating = rms.copy()
+    rms_floating[mask==2] = 0.
 
-    elif restrict_to == "none":
-        pass
+    rms_grounded = rms.copy()
+    rms_grounded[mask==3] = 0.
 
-    else:
-        raise NotImplemented
-
-    return rms
+    return {"rms_sum":rms.sum(),
+            "rms_sum_grounded":rms_grounded.sum(),
+            "rms_sum_floating":rms_floating.sum()}
 
 
 def get_summed_rms_error(fname, varname, reference_field,
                          restrict_to="none"):
 
     """ use the fields from fname to calculate rms against
-        reference field. sum up the rms and return.
-        restrict_to can be passed to the get_spatial_rms_error
-        function.
+        reference field.
     """
 
     variable = get_spatial_variable(fname, varname)
     mask = get_spatial_variable(fname, "mask")
 
-    rms_of_var = get_spatial_rms_error(variable,reference_field, mask,
-                 restrict_to=restrict_to)
+    rms_of_var = get_rms_error(variable,reference_field, mask,
+                 spatial = False)
 
-    return rms_of_var.sum()
+    return rms_of_var
