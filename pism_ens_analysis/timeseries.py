43e08d52937a72d9414670d3e7f17b1159cb0d22
Matthias Mengel <mengel@pik-potsdam.de>
2017-08-14 14:00:00 +0200

    get_timeseries_data2 now handles multiple ts files.

--- pism_ens_analysis/timeseries.py
+++ pism_ens_analysis/timeseries.py
@@ -4,61 +4,26 @@ import numpy as np
 # import pandas as pd
 import netCDF4 as nc
 import dimarray as da
+import glob
 
 
-def get_timeseries_data(datapath, ts_variables = ["slvol","area_glacierized_shelf"],
-                        ts_file_name="timeseries.nc"):
-
-    """ get time series variables ts_variables from PISM timeseries
-    file in datapath.
-    TODO: merge with get_timeseries_data2
-    """
-
-    name = datapath.split("/")[-1]
-
-    ts_data = {}
-
-    try:
-        ncf = nc.Dataset(os.path.join(datapath,ts_file_name),"r")
-    except IOError as error:
-        print name, "has no timeseries file, skip"
-        # raise error
-    try:
-        nct = ncf.variables["time"]
-    except KeyError as error:
-        print name, "contains no data, skip."
-        # raise error
-
-    for var in ts_variables:
-        ts_data[var] = ncf.variables[var][:]
-    ts_data = da.Dataset(ts_data)
-
-    datetm = nc.num2date(nct[:],units = nct.units,calendar = nct.calendar)
-    # takes long for long timeseries
-    years = [d.year for d in datetm]
-    ts_data.set_axis(years)
-    ts_data.rename_axes({"x0":"time"})
-
-    return ts_data
-
-
-def get_netcdf_as_dataset(spath,ts_file_name):
+def get_netcdf_as_dataset(ts_file_name):
 
     """ Read all timeseries variable from ts_file_name
     into one dimarray Dataset. Handle some errors if it
     not existent or empty. """
 
     try:
-        ncf = nc.Dataset(os.path.join(spath,ts_file_name),"r")
+        ncf = nc.Dataset(ts_file_name,"r")
     except IOError as e:
         print spath, "has no", ts_file_name, "file, skip"
-        raise IOError(spath + " has no "+ ts_file_name)
+        raise IOError(ts_file_name + "does not exist.")
 
     try:
         nct = ncf.variables["time"]
     except KeyError:
         print name, "contains no data, skip."
-        raise KeyError(spath+ts_file_name+" contains no data.")
+        raise KeyError(ts_file_name+" contains no data.")
 
     ts_variables = ncf.variables.keys()
     ## ignore some variables
@@ -81,23 +46,37 @@ def get_netcdf_as_dataset(spath,ts_file_name):
     return dataset
 
 
-def get_timeseries_data2(ensemble_members, ts_file_name="timeseries.nc"):
+def get_timeseries_data2(ensemble_members, ts_file_pattern="timeseries"):
 
-    """ loop over all ensemble members, check if they have a ts_file_name,
-    read its data and save it to an ordered dictionary.
+    """ loop over all ensemble members, get their ts file data,
+        merge them and collect them into an ordered dict.
     """
 
     ensemble_data = {}
-    for em in ensemble_members:
-        name = em.split("/")[-1]
-        try:
-            ensemble_data[name] = get_netcdf_as_dataset(em,ts_file_name)
-        except (IOError, KeyError) as e:
-            print e
-            continue
 
-    # keep it ordered
+    for em in ensemble_members:
+
+        name = em.split("/")[-1]
+        # these are the files like timeseries_308000.nc from restarts
+        available_ts_restart_files = sorted(glob.glob(
+            os.path.join(em,ts_file_pattern+"_*nc")))
+
+        # append the timeseries.nc files
+        available_ts_files = available_ts_restart_files + [
+            os.path.join(em,ts_file_pattern+".nc")]
+
+        for ts_file in available_ts_files:
+
+            if ts_file == available_ts_files[0]:
+                dataset = get_netcdf_as_dataset(ts_file)
+            else:
+                ds = get_netcdf_as_dataset(ts_file)
+                dataset = da.concatenate_ds((dataset,ds),axis="time")
+
+        ensemble_data[name] = dataset
+
     ensemble_data = collections.OrderedDict(sorted(ensemble_data.items()))
+
     return ensemble_data
 
 
